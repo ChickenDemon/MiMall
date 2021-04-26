9d1ec50480886afaa8d0e7cfbb979f748f936b77
ChickenDemon <1097203087@qq.com>
2021-04-26 23:58:40 +0800

    v3-7:路由封装

---
+++ src/router.js
@@ -0,0 +1,73 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import Home from './pages/home';
+import Index from './pages/index';
+import Product from './pages/product/index';
+import Detail from './pages/product/detail';
+import Cart from './pages/cart/index';
+import Order from './pages/order/index';
+import OrderConfirm from './pages/order/orderConfirm';
+import OrderList from './pages/order/orderList';
+import OrderPay from './pages/order/pay';
+import Alipay from './pages/order/alipay';
+
+Vue.use(Router);
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home,
+      children: [
+        {
+          path: '/index',
+          name: 'index',
+          component: Index,
+        },
+        {
+          path: 'product/index/:id',
+          name: 'product-index',
+          component: Product,
+        },
+        {
+          path: 'product/detail/:id',
+          name: 'detail',
+          component: Detail,
+        },
+      ]
+    },
+    {
+      path: '/cart',
+      name: 'cart',
+      component: Cart
+    },
+    {
+      path: '/order',
+      name: 'order',
+      component: Order,
+      children: [
+        {
+          path: 'orderConfirm',
+          name: 'order-confirm',
+          component: OrderConfirm
+        },
+        {
+          path: 'orderList',
+          name: 'order-list',
+          component: OrderList
+        },
+        {
+          path: 'pay',
+          name: 'order-pay',
+          component: OrderPay
+        },
+        {
+          path: 'alipay',
+          name: 'alipay',
+          component: Alipay
+        },
+      ]
+    },
+  ]
+});
